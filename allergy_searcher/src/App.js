a8ccfa89fe186dae40f3f310978c565c8d1018e2
Andrea <90292543+amgallo1213@users.noreply.github.com>
2023-04-06 22:48:24 -0400

    working on functionality

--- allergy_searcher/src/App.js
+++ allergy_searcher/src/App.js
@@ -1,10 +1,13 @@
-import SearchBar from './components/SearchBar';
+import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Search for a Menu Item</h1>
+      <SearchBar />
     </div>
   );
 }
--- allergy_searcher/src/components/SearchBar.js
+++ allergy_searcher/src/components/SearchBar.js
@@ -1,37 +1,55 @@
 import React, { useState } from 'react';
 
-const SearchBar = (MenuItem) => {
+const SearchBar = () => {
 
     const [searchInput, setSearchInput] = useState("");
 
-    const menuItems = [
-        {name: "salsa verde", allergens: "garlic, onion, cilantro" },
-        {name: "salsa roja", allergens: "garlic, onion, cilantro, soy" },
-        {name: "El Vez Guac", allergens: "garlic, onion, cilantro, soy, citrus, nightshade" },
-        {name: "Speciality Guac Base", allergens: "citrus" },
-        {name: "De La Hoya", allergens: "garlic, onion, soy, citrus" },
-        {name: "Indian Red Lopez", allergens: "gluten, bivalves, shellfish, onion, cilantro" },
-        {name: "Bazooka Limon", allergens: "garlic, onion, dairy, tree nuts, nightshades" },
-        {name: "Tito Santana", allergens: "garlic, onion, cilantro, mango, bell pepper" },
-        {name: "salsa Mexicana", allergens: "onion, cilantro, soy, citrus, nightshade, alcohol" }
-    ]
+        const menuItems = [
+            {name: "salsa verde", allergens: "garlic, onion, cilantro" },
+            {name: "salsa roja", allergens: "garlic, onion, cilantro, soy" },
+            {name: "El Vez Guac", allergens: "garlic, onion, cilantro, soy, citrus, nightshade" },
+            {name: "Speciality Guac Base", allergens: "citrus" },
+            {name: "De La Hoya", allergens: "garlic, onion, soy, citrus" },
+            {name: "Indian Red Lopez", allergens: "gluten, bivalves, shellfish, onion, cilantro" },
+            {name: "Bazooka Limon", allergens: "garlic, onion, dairy, tree nuts, nightshades" },
+            {name: "Tito Santana", allergens: "garlic, onion, cilantro, mango, bell pepper" },
+            {name: "salsa Mexicana", allergens: "onion, cilantro, soy, citrus, nightshade, alcohol" }
+        ]
 
-    const handleChange = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value);
-    };
+        const handleChange = (e) => {
+            e.preventDefault();
+            setSearchInput(e.target.value);
+        };
 
-    if (searchInput.length > 0) {
-        menuItems.filter((item) => {
-            return item.name.match(searchInput);
-        });
+        if (menuItems.length > 0) {
+            menuItems.filter((item) => {
+                return item.name.match(menuItems);
+            });
     }
 
     return ( 
         <div>
             
+            <input
+                type="search"
+                placeholder="Search here"
+                onChange={handleChange}
+                value={searchInput} />
+
+            {menuItems.map((item, index) => (
+
+               
+                <div>
+                    <p>{item.name}</p>
+                    <p>{item.allergens}</p>
+                </div>
+               
+
+            ))}
+
+
         </div>
-     );
+    );
 }
  
 export default SearchBar;
